c9d9479df9685eb57d2868919cb43ea2626c8006
Raul Matheus dos Santos Rodrigues <raulth824@gmail.com>
2020-04-03 17:06:29 -0300

    Iniciei o projeto, quebramdo a cabeça

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,3 +10,20 @@
  * 
  * 
  */
+console.log("Bem vindo ao jogo BlackJack!!!")
+
+if(confirm("Quer iniciar o jogo?")){
+
+}else{
+   console.log("O jogo acabou")
+}
+
+// const carta = comprarCarta(); 
+// // Sorteia uma carta. Por exemplo, o rei de ouros
+
+
+// console.log(carta.texto) 
+// // imprime o texto da carta. Nesse caso: "K♦️"
+
+// console.log(carta.valor) 
+// // imprime o valor da carta (um número). Nesse caso: 10)
